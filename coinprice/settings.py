014907783974d5499655f8f20f2dc9261a9357ea
samzhuwj <samzhuwj@gmail.com>
2017-12-06 22:02:42 +0800

    use rediscloud_url

--- coinprice/settings.py
+++ coinprice/settings.py
@@ -136,7 +136,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'asgi_redis.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            'hosts': [os.environ.get('REDIS_URL', config('REDISCLOUD_URL', default='redis://localhost:6379'))],
             'capacity': 100,
         },
         'ROUTING': 'bitcoinmonitor.routing.channel_routing',
